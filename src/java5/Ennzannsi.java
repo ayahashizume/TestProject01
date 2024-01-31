b31de0cf2da41d0b672773f9c1d787a2de463b3e
はしづめ <156087872+ayahashizume@users.noreply.github.com>
2024-01-31 22:28:02 +0900

    2401312227

---
+++ src/java5/Ennzannsi.java
@@ -0,0 +1,12 @@
+package java5;
+
+public class Ennzannsi {
+
+	public static void main(String[] args) {
+	int a = 3+3;
+	System.out.println(a);
+	
+
+	}
+
+}
---
+++ src/java5/Iroiroennzann.java
@@ -0,0 +1,14 @@
+package java5;
+
+public class Iroiroennzann {
+
+	public static void main(String[] args) {
+		int a1 =4+3;
+		System.out.println(a1);
+		
+		int a2 =-8-3;
+		System.out.println(a2);
+
+	}
+
+}
