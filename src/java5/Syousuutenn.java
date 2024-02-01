92e59a95fa88074709c84076b5e6b692975248be
はしづめ <156087872+ayahashizume@users.noreply.github.com>
2024-02-01 22:10:49 +0900

    2402012210

---
+++ src/java5/Syousuutenn.java
@@ -0,0 +1,14 @@
+package java5;
+
+public class Syousuutenn {
+
+	public static void main(String[] args) {
+		int a = 10/3;
+		System.out.println(a);
+		
+		float a2 = (float)10/3;
+		System.out.println(a2);
+
+	}
+
+}
