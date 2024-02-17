dae9fc87ee64eca786e3745c594dc474fba7ec64
はしづめ <156087872+ayahashizume@users.noreply.github.com>
2024-02-17 20:57:55 +0900

    240217

---
+++ src/java7/Ifa1.java
@@ -0,0 +1,14 @@
+package java7;
+
+public class Ifa1 {
+
+	public static void main(String[] args) {
+		int b = 18;
+		if(b>= 20) {
+			System.out.println("成人");
+		}else {
+			System.out.println("未成年");
+		}
+	}
+
+}
---
+++ src/java7/Ifa4.java
@@ -0,0 +1,15 @@
+package java7;
+
+public class Ifa4 {
+
+	public static void main(String[] args) {
+		int a =5;
+		if(a%2 ==0) {
+			System.out.println("偶数");
+		}else{
+			System.out.println("奇数");
+		}
+
+	}
+
+}
