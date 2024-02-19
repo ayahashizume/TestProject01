aaef44b4850561a6475a82efee71a2351a6f3e03
はしづめ <156087872+ayahashizume@users.noreply.github.com>
2024-02-19 21:59:15 +0900

    2402192158

---
+++ src/java11/Tajigenn1.java
@@ -0,0 +1,15 @@
+package java11;
+
+public class Tajigenn1 {
+
+	public static void main(String[] args) {
+	String[][] country =
+		{
+				{"日本","タイ"},
+				{"アメリカ","ブラジル"},
+				{"フランス","ロシア"}
+		};
+	System.out.println( country[2][1]);
+	}
+
+}
